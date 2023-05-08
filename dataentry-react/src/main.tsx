132a2b98cc837d1d86e19a208c99cf9329c394f1
Kazi tanvir Azad <kazitanvir.azad@publicissapient.com>
2023-05-08 14:25:32 +0530

    React test1

--- dataentry-react/src/main.tsx
+++ dataentry-react/src/main.tsx
@@ -1,31 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './Home.tsx'
+// import Home from './Home.tsx'
 import 'bootstrap/dist/css/bootstrap.css';
-import AddProjectDataFormPage from './components/AddProjectDataFormPage.tsx';
-import ListProjectTable from './components/ListProjectTable.tsx';
-import SearchProjectForm from './components/SearchProjectForm.tsx'
+// import AddProjectDataFormPage from './components/AddProjectDataFormPage.tsx';
+// import ListProjectTable from './components/ListProjectTable.tsx';
+// import SearchProjectForm from './components/SearchProjectForm.tsx'
 import App from './App.tsx';
 
 document.title = 'Welcome | Data Entry';
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider
-} from "react-router-dom";
+// import {
+//   createBrowserRouter,
+//   createRoutesFromElements,
+//   Route,
+//   RouterProvider
+// } from "react-router-dom";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route path="/" element={<Home />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/addproject" element={<AddProjectDataFormPage />} />
-      <Route path="/projectlist" element={<ListProjectTable />} />
-      <Route path="/searchprojects" element={<SearchProjectForm />} />
-    </>
-  )
-);
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <>
+//       <Route path="/" element={<Home />} />
+//       <Route path="/home" element={<Home />} />
+//       <Route path="/addproject" element={<AddProjectDataFormPage />} />
+//       <Route path="/projectlist" element={<ListProjectTable />} />
+//       <Route path="/searchprojects" element={<SearchProjectForm />} />
+//     </>
+//   )
+// );
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
